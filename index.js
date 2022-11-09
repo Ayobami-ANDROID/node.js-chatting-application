dd6b60f48d075c67dde76606255863980de3fa19
Ayobami Ajetunmobi <ajetunmobiayobami19@gmail.com>
2022-11-09 22:24:50 +0100

    chattingApp

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+const express = require("express");
+const socket = require("socket.io");
+
+// App setup
+const PORT = 5000;
+const app = express();
+const server = app.listen(PORT, function () {
+  console.log(`Listening on port ${PORT}`);
+  console.log(`http://localhost:${PORT}`);
+});
+
+// Static files
+app.use(express.static("public"));
+
+// Socket setup
+const io = socket(server);
+
+io.on("connection", function (socket) {
+  console.log("Made socket connection");
+});
