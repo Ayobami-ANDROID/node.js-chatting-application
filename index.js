081bc52ae41e2a67068069e676b494ee71926774
Ayobami Ajetunmobi <ajetunmobiayobami19@gmail.com>
2022-11-09 23:28:34 +0100

    chat app

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
+const io = require('socket.io')(3000)
 
+const users = {}
 
-const express = require("express");
-const socket = require("socket.io");
-
-// App setup
-const PORT = 5000;
-const app = express();
-const server = app.listen(PORT, function () {
-  console.log(`Listening on port ${PORT}`);
-  console.log(`http://localhost:${PORT}`);
-});
-
-// Static files
-app.use(express.static("public"));
-
-// Socket setup
-const io = socket(server);
-
-io.on("connection", function (socket) {
-  console.log("Made socket connection");
-});
+io.on('connection', socket => {
+  socket.on('new-user', name => {
+    users[socket.id] = name
+    socket.broadcast.emit('user-connected', name)
+  })
+  socket.on('send-chat-message', message => {
+    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
+  })
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('user-disconnected', users[socket.id])
+    delete users[socket.id]
+  })
+})
